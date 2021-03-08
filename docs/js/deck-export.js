edd86cf47fdc3a5ce1c14fce76c829a1c2a9e894
Mani <12841290+infinyte7@users.noreply.github.com>
2021-03-09 04:33:44 +0800

    Update deck-export.js

--- docs/js/deck-export.js
+++ docs/js/deck-export.js
@@ -23,7 +23,8 @@ if new_title != None:
 
 anki_model_name = "ocloze-infi"
 
-model_id = random.randrange(1 << 30, 1 << 31)
+# model_id = random.randrange(1 << 30, 1 << 31)
+model_id = 1918965134
 
 def exportDeck(data_filename, deck_filename):
    try:
