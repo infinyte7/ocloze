00cd2cdb69c2712f279785fc997bb71f40fd0d9d
Mani <12841290+infinyte7@users.noreply.github.com>
2021-03-09 02:08:51 +0800

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -300,19 +300,6 @@ function showHelp() {
     }
 }
 
-// https://stackoverflow.com/questions/6604192/showing-console-errors-and-alerts-in-a-div-inside-the-page
-if (typeof console != "undefined")
-    if (typeof console.log != 'undefined')
-        console.olog = console.log;
-    else
-        console.olog = function () { };
-
-console.log = function (message) {
-    console.olog(message);
-    document.getElementById("pyodide-load-msg").innerHTML += '<p>' + message + '</p>';
-};
-console.error = console.debug = console.info = console.log
-
 function closeConsole() {
     document.getElementById("pyodide-load-status").style.display = "none";
 }
